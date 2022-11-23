4788a7ebbde3c9a4c94f320717410ad5ffd94411
doughertybd <doughebd@mail.uc.edu>
2022-11-23 12:28:14 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+/*        for(int cnt = 1; cnt <= 30; cnt++)
+        {
+            System.out.println(cnt);
+        }
+
+*/
+
+/*        for(int cnt = 30; cnt >= 1; cnt--)
+        {
+            System.out.println(cnt);
+        }
+
+ */
+
+ /*       for(int cnt = 0; cnt <= 18; cnt = cnt + 3)
+        {
+            System.out.println(cnt);
+        }
+
+  */
+
+        for(int cnt = 10; cnt >= 1; cnt = cnt - 2)
+        {
+            System.out.println(cnt);
+        }
+
+    }
+}
